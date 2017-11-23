c0ddbf56744e14bd0d831ffe74d7df061cb41870
John Angelo Barot <jangelo08@asduff.local>
2017-11-23 15:09:28 +0800

    updated structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <head-component/>
+      <router-view/>
+    <!-- <foot-component/> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style lang="scss">
+   @import './scss/main.scss';
+</style>
